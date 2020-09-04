974c4c59a9b87774499897be238df4bfaa50d05b
viveksenthil3 <svivek.hosur5@gmail.com>
2020-09-04 23:57:22 +0530

    fill cart from session storage

--- src/public/customer/js/cart.js
+++ src/public/customer/js/cart.js
@@ -1,1 +1,13 @@
-$
+$(document).ready(_ => {
+    const dishes = JSON.parse(sessionStorage.getItem('dishes'));
+    console.log(dishes);
+
+    const dishesContainer = $('.dishes-selected');
+    let billAmount=0;
+    dishes.forEach(dish=>{
+        dishesContainer.append(`<li><span class="dish-name">${ dish.name }</span> - &#8377<span class="dish-price">${ dish.price }</span> x <span class="dish-quantity">${ dish.qunt }</span> = &#8377<span class="dish-totprice">${ dish.totPrice }</span></li>`);
+        billAmount+=parseInt(dish.totPrice);
+    })
+    
+    $('.bill-amount').html(billAmount);
+})
